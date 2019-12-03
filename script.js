8b4b5c9b13c06ecfb661716ae97633836d05d392
james davies <james.davies92@icloud.com>
2019-12-03 17:26:34 +0000

    adderd more styling and pseudo code for what has left to be done in the js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //---------------------------------------------DAY PLANNER-----------------------------------------------------
 
+//current date display in header of page
 var today = moment();
 
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -7,6 +8,7 @@ console.log("This is what the moment .day prints out", console.log(today.day()))
 var currentDay = daysOfWeek[today.day()];
 document.getElementById('currentdate').innerHTML += currentDay;
 
+//on click listener when clicking save button, saves user input field
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
         var value = $(this).siblings(".description").val();
@@ -15,6 +17,7 @@ $(document).ready(function () {
     })
 });
 
+//function that creates new html classes so we can style via css to display time of day
 function hourUpdater() {
     // get current number of hours
     var currentHour = moment().hours();
@@ -37,7 +40,7 @@ function hourUpdater() {
     });
 }
 hourUpdater();
-
+//local storage saving for all user inputs
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
@@ -47,3 +50,13 @@ $("#14 .description").val(localStorage.getItem("14"));
 $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
+
+
+
+
+//THESE THINGS ARE STILL MISSING FROM PAGE
+
+//-text inputs need to be in ul/li bullet points
+//-text box needs to expand in size when input field becomes too small for text
+//-colour changing times to show what is past/present and future
+//
